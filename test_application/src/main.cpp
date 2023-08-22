93b5a1afea2f36e8cb197672714498f83bebb824
Camilo <crlimacastro@gmail.com>
2023-08-22 00:11:27 -0400

    created starte test application

--- test_application/src/main.cpp
+++ test_application/src/main.cpp
@@ -1,6 +1,14 @@
 ﻿#include "fae_framework.hpp"
+#include <GLFW/glfw3.h>
 
 int main()
 {
+	fae::ecs_application test_application{};
+	test_application.run();
 
+	glfwInit();
+	fae::glfw_window test_window{};
+	do {
+		glfwPollEvents();
+	} while (!test_window.should_close());
 }
